0e4bfef6467b61c52b601eda849d8fd5ee342f19
Duncan Walter <duncan1997@hotmail.nl>
2025-05-27 15:25:52 +0200

    implemented remaining fruit counters

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import FruitCounter from "./components/FruitCounter.jsx";
 
 function App() {
   const [strawBerryAmountState, setStrawBerryAmountState] = useState(0);
+  const [bananaAmountState, setBananaAmountState] = useState(0);
+  const [appleAmountState, setAppleAmountState] = useState(0);
+  const [kiwiAmountState, setKiwiAmountState] = useState(0);
 
   return (<>
     <h1>Fruitmand bezorgservice</h1>
@@ -12,7 +15,21 @@ function App() {
       state={strawBerryAmountState}
       setState={setStrawBerryAmountState}
     />
-    🍌🍏🥝
+    <FruitCounter
+      title="🍌 Bananen"
+      state={bananaAmountState}
+      setState={setBananaAmountState}
+    />
+    <FruitCounter
+      title="🍏 Appels"
+      state={appleAmountState}
+      setState={setAppleAmountState}
+    />
+    <FruitCounter
+      title="🥝 Kiwi's"
+      state={kiwiAmountState}
+      setState={setKiwiAmountState}
+    />
   </>)
 }
 
