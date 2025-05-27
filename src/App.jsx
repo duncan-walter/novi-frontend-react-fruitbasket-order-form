534bd0d84ded96dca7de1c81563258cf3c83952e
Duncan Walter <duncan1997@hotmail.nl>
2025-05-27 15:11:05 +0200

    implemented simple componentless fruit counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-
 import './App.css'
+import { useState } from 'react';
 
 function App() {
+  const [strawBerryAmountState, setStrawBerryAmountState] = useState(0);
 
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+  function handleFruitAmountMutation(setCallback, currentAmount, mutationAmount) {
+    if (currentAmount + mutationAmount > -1) {
+      setCallback(currentAmount + mutationAmount);
+    }
+  }
+
+  return (<>
+    <h1>Fruitmand bezorgservice</h1>
+    <div>
+      <span>🍓 Aardbeien</span>
+      <button onClick={() => { handleFruitAmountMutation(setStrawBerryAmountState, strawBerryAmountState, -1)}}>-</button>
+      <span>{strawBerryAmountState}</span>
+      <button onClick={() => { handleFruitAmountMutation(setStrawBerryAmountState, strawBerryAmountState, 1)}}>+</button>
+    </div>🍌🍏🥝
+  </>)
 }
 
 export default App
