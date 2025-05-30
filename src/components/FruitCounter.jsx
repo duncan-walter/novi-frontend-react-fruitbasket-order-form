9178c330eb6fb2df6168893002796509d3fb1862
Duncan Walter <duncan1997@hotmail.nl>
2025-05-30 16:07:36 +0200

    added conditional styling to fruit counters

--- src/components/FruitCounter.jsx
+++ src/components/FruitCounter.jsx
@@ -24,7 +24,7 @@ function FruitCounter({title, state, setState, name}) {
   }
 
   return (
-    <div className="fruit-counter">
+    <div className={state > minimumAmount ? 'fruit-counter fruit-counter-active' : 'fruit-counter'}>
       <span className="fruit-counter-title">{title}</span>
       <Button text="-" type="button" action={decreaseFruitAmount}/>
       <span className="fruit-counter-amount">{state}</span>
