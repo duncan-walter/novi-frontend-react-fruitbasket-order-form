10018ed2bd2f8544058a4ee156eac26f454ce014
duncan-walter <duncan1997@hotmail.nl>
2025-06-03 21:36:42 +0200

    simplified FruitCounter component

--- src/components/FruitCounter.jsx
+++ src/components/FruitCounter.jsx
@@ -3,33 +3,13 @@ import './FruitCounter.css';
 import Button from './Button.jsx';
 
 function FruitCounter({title, state, setState, name}) {
-  const increaseAmount = 1;
-  const decreaseAmount = -1;
-  const minimumAmount = 0;
-
-  function increaseFruitAmount() {
-    handleFruitAmountMutation(state, setState, increaseAmount);
-  }
-
-  function decreaseFruitAmount() {
-    handleFruitAmountMutation(state, setState, decreaseAmount);
-  }
-
-  function handleFruitAmountMutation(state, setState, mutationAmount) {
-    if (state + mutationAmount > minimumAmount - 1) {
-      setState(name, state + mutationAmount);
-    } else {
-      setState(name, minimumAmount);
-    }
-  }
-
   return (
-    <div className={state > minimumAmount ? 'fruit-counter fruit-counter-active' : 'fruit-counter'}>
+    <div className={state > 0 ? 'fruit-counter fruit-counter-active' : 'fruit-counter'}>
       <span className="fruit-counter-title">{title}</span>
       <div className="fruit-counter-controls">
-        <Button text="-" type="button" handleClick={decreaseFruitAmount}/>
+        <Button text="-" type="button" handleClick={() => setState(name, state === 0 ? state : state - 1)}/>
         <span className="fruit-counter-amount">{state}</span>
-        <Button text="+" type="button" handleClick={increaseFruitAmount}/>
+        <Button text="+" type="button" handleClick={() => setState(name, state + 1)}/>
       </div>
     </div>
   );
